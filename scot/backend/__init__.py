c0dd471ce3d53dbbf3a0763f7a0b5e209c533800
Martin Billinger <martin.billinger@tugraz.at>
2013-10-24 11:36:04 +0200

    Added information about backends for packagers
    Former-commit-id: c919f78ad9728216c4d598fb1a69088d48b7ef98

--- scot/backend/__init__.py
+++ scot/backend/__init__.py
@@ -2,4 +2,6 @@
 # http://opensource.org/licenses/MIT
 # Copyright (c) 2013 SCoT Development Team
 
+"""To remove backends from your distribution, edit this list."""
+
 __all__ = ['sklearn', 'builtin']
