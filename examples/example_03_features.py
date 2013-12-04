f3bcd349381a80699657d91cbdea89110321b7d6
Martin <martin.billinger@tugraz.at>
2013-12-04 15:23:37 +0100

    Examples : cross-validation and classification example
    Former-commit-id: 7fa93f3edb8207ba1740415c451e44cf2e1e677a

--- examples/example_03_features.py
+++ examples/example_03_features.py
@@ -3,6 +3,8 @@ This example shows how to decompose EEG signals into source activations with MVA
 """
 import numpy as np
 from sklearn.lda import LDA
+from sklearn.cross_validation import KFold
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
 import scot
@@ -29,6 +31,7 @@ locs = midata.locations
 # Set up the analysis object
 # We simply choose a VAR model order of 30, and reduction to 4 components.
 ws = scot.Workspace(30, reducedim=4, fs=fs)
+freq = np.linspace(0, fs, ws.nfft_)
 
 
 # Prepare the data
@@ -37,40 +40,51 @@ ws = scot.Workspace(30, reducedim=4, fs=fs)
 # is right in the middle of the motor imagery period.
 data = scot.datatools.cut_segments(raweeg, triggers, 3 * fs, 4 * fs)
 
-
-# Perform MVARICA
-ws.set_data(data)
-ws.do_mvarica()
-
-# Find optimal regularization parameter for single-trial fitting
-#ws.optimize_regularization(scot.xvschema.singletrial, 30)
-
-freq = np.linspace(0, fs, ws.nfft_)
-
-# Single-Trial Fitting and feature extraction
-features = np.zeros((len(triggers), 32))
-for t in range(len(triggers)):
-    print('Trial: %d   ' % t, end='\r')
-    ws.set_data(data[:, :, t])
-    ws.fit_var()
-
-    con = ws.get_connectivity('ffPDC')
-
-    alpha = np.mean(con[:, :, np.logical_and(7 < freq, freq < 13)], axis=2)
-    beta = np.mean(con[:, :, np.logical_and(15 < freq, freq < 25)], axis=2)
-
-    features[t, :] = np.array([alpha, beta]).flatten()
-print('')
+# Initialize Cross Validation
+nfolds = 10
+kf = KFold(len(triggers), n_folds=nfolds, indices=False)
 
 # LDA requires numeric class labels
 clunique = np.unique(midata.classes)
-classids = [dict( zip(clunique,range(len(clunique))))[c] for c in midata.classes]
+classids = np.array([dict(zip(clunique, range(len(clunique))))[c] for c in midata.classes])
 
-
+# Perform Cross Validation
 lda = LDA()
-lda.fit(features, classids)
+cm = np.zeros((2, 2))
+fold = 0
+for train, test in kf:
+    fold += 1
 
-llh = lda.transform(features)
+    # Perform MVARICA
+    ws.set_data(data[:, :, train], classes[train])
+    ws.do_mvarica()
 
-plt.hist([llh[classes == 'hand', :], llh[classes == 'foot', :]])
-plt.show()
+    # Find optimal regularization parameter for single-trial fitting
+    #ws.optimize_regularization(scot.xvschema.singletrial, 30)
+
+    # Single-Trial Fitting and feature extraction
+    features = np.zeros((len(triggers), 32))
+    for t in range(len(triggers)):
+        print('Fold %d/%d, Trial: %d   ' %(fold, nfolds, t), end='\r')
+        ws.set_data(data[:, :, t])
+        ws.fit_var()
+
+        con = ws.get_connectivity('ffPDC')
+
+        alpha = np.mean(con[:, :, np.logical_and(7 < freq, freq < 13)], axis=2)
+        beta = np.mean(con[:, :, np.logical_and(15 < freq, freq < 25)], axis=2)
+
+        features[t, :] = np.array([alpha, beta]).flatten()
+
+    lda.fit(features[train, :], classids[train])
+
+    acc_train = lda.score(features[train, :], classids[train])
+    acc_test = lda.score(features[test, :], classids[test])
+
+    print('Fold %d/%d, Acc Train: %.4f, Acc Test: %.4f' %(fold, nfolds, acc_train, acc_test))
+
+    pred = lda.predict(features[test, :])
+    cm += confusion_matrix(classids[test], pred)
+print('Confusion Matrix:\n', cm)
+
+print('Total Accuracy: %.4f'%(np.sum(np.diag(cm))/np.sum(cm)))
