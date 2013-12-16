SCoT : fixed off-by-one error
Former-commit-id: 5c8b1c5d2cc7c41b9e7cad199e6755e8c4c4072b

scot/utils.py
@@ -46,7 +46,7 @@ def acm(x, l):
     """
     x = np.atleast_3d(x)
 
-    if l > x.shape[0]-2:
+    if l > x.shape[0]-1:
         raise AttributeError("lag exceeds data length")
 
     ## subtract mean from each trial