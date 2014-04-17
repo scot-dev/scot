9e226746e7a2e68806508203529eda45e6489968
Martin <martin.billinger@tugraz.at>
2014-04-17 17:54:51 +0200

    Falling back to builtin cartesian if it is not available from sklearn

--- scot/backend/sklearn_utils.py
+++ scot/backend/sklearn_utils.py
@@ -5,4 +5,11 @@ from __future__ import absolute_import
 
 import sklearn.utils.extmath as extmath
 
-cartesian = extmath.cartesian
+if hasattr(extmath, 'cartesian_'):
+    cartesian = extmath.cartesian
+else:
+    # fall back to builtin cartesian since older
+    # sklearn versions don't have this function.
+    from ..builtin import utils
+    cartesian = utils.cartesian
+
