6fbba49640dc8e009dd5f3dd0e19cd5139951f5b
Martin Billinger <martin.billinger@tugraz.at>
2014-04-23 01:08:24 +0200

    Disabled examples again

--- distributions/ci/run_tests.sh
+++ distributions/ci/run_tests.sh
@@ -18,8 +18,8 @@ fi
 
 if [[ "$RUN_EXAMPLES" == "true" ]]; then
     if [[ "$INSTALL_SCOT" == "true" ]]; then
-        xvfb-run --server-args="-screen 0 1024x768x24" find ../examples -type f -iname "*\.py" -exec python -v {} \;
+        xvfb-run --server-args="-screen 0 1024x768x24" find ../examples -type f -iname "*\.py" -exec python {} \;
     else
-        PYTHONPATH=. xvfb-run --server-args="-screen 0 1024x768x24" find examples -type f -iname "*\.py" -exec python -v {} \;
+        PYTHONPATH=. xvfb-run --server-args="-screen 0 1024x768x24" find examples -type f -iname "*\.py" -exec python {} \;
     fi
 fi
