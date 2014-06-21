755cd80b8e351395a8d67693daa17508e7d02820
Martin Billinger <flkazemakase@gmail.com>
2014-06-21 11:15:15 +0200

    some more parallel tests

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -9,6 +9,14 @@ def f(x):
     return x**2 - 1
 
 
+def g(x, y, z):
+    return x**y - z
+
+
+def h(x):
+    return x, x**2
+
+
 class TestFunctions(unittest.TestCase):
     def setUp(self):
         pass
@@ -23,30 +31,62 @@ class TestFunctions(unittest.TestCase):
 
         # reference list comprehension
         ref = [f(i) for i in range(10)]
+        reg = [g(i, j, 5) for i, j in enumerate(range(10, 20))]
+        reh = [h(i) for i in range(10)]
 
         # test non-parallel execution
         par, func = parallel_loop(f, n_jobs=None, verbose=verbose)
         self.assertEqual(ref, par(func(i) for i in range(10)))
+        # multiple arguments
+        par, func = parallel_loop(g, n_jobs=None, verbose=verbose)
+        self.assertEqual(reg, par(func(i, j, 5) for i, j in enumerate(range(10, 20))))
+        # multiple return values
+        par, func = parallel_loop(h, n_jobs=None, verbose=verbose)
+        self.assertEqual(reh, par(func(i) for i in range(10)))
 
         # test non-parallel execution with joblib
         par, func = parallel_loop(f, n_jobs=1, verbose=verbose)
         b = par(func(i) for i in range(10))
         self.assertEqual(ref, par(func(i) for i in range(10)))
+        # multiple arguments
+        par, func = parallel_loop(g, n_jobs=1, verbose=verbose)
+        self.assertEqual(reg, par(func(i, j, 5) for i, j in enumerate(range(10, 20))))
+        # multiple return values
+        par, func = parallel_loop(h, n_jobs=1, verbose=verbose)
+        self.assertEqual(reh, par(func(i) for i in range(10)))
 
         # test parallel execution with joblib
         par, func = parallel_loop(f, n_jobs=2, verbose=verbose)
         b = par(func(i) for i in range(10))
         self.assertEqual(ref, par(func(i) for i in range(10)))
+        # multiple arguments
+        par, func = parallel_loop(g, n_jobs=2, verbose=verbose)
+        self.assertEqual(reg, par(func(i, j, 5) for i, j in enumerate(range(10, 20))))
+        # multiple return values
+        par, func = parallel_loop(h, n_jobs=2, verbose=verbose)
+        self.assertEqual(reh, par(func(i) for i in range(10)))
 
         # test parallel execution with joblib
         par, func = parallel_loop(f, n_jobs=-1, verbose=verbose)
         b = par(func(i) for i in range(10))
         self.assertEqual(ref, par(func(i) for i in range(10)))
+        # multiple arguments
+        par, func = parallel_loop(g, n_jobs=-1, verbose=verbose)
+        self.assertEqual(reg, par(func(i, j, 5) for i, j in enumerate(range(10, 20))))
+        # multiple return values
+        par, func = parallel_loop(h, n_jobs=-1, verbose=verbose)
+        self.assertEqual(reh, par(func(i) for i in range(10)))
 
         # test parallel execution with joblib
         par, func = parallel_loop(f, n_jobs=10, verbose=verbose)
         b = par(func(i) for i in range(10))
         self.assertEqual(ref, par(func(i) for i in range(10)))
+        # multiple arguments
+        par, func = parallel_loop(g, n_jobs=10, verbose=verbose)
+        self.assertEqual(reg, par(func(i, j, 5) for i, j in enumerate(range(10, 20))))
+        # multiple return values
+        par, func = parallel_loop(h, n_jobs=10, verbose=verbose)
+        self.assertEqual(reh, par(func(i) for i in range(10)))
 
 
 if __name__ == '__main__':
