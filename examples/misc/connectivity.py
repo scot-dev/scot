55c3a7b2672631c5264ccd706212b8f30205f2e4
Clemens Brunner <clemens.brunner@gmail.com>
2015-01-09 12:17:21 +0100

    PEP8 and minor text modifications.

--- examples/misc/connectivity.py
+++ examples/misc/connectivity.py
@@ -1,18 +1,19 @@
 """
 This example shows how to decompose EEG signals into source activations with
-MVARICA, and visualize a connectivity.
+CSPVARICA, and visualize a connectivity.
 """
 
 import scot
 
-# The example data set contains a continuous 45 channel EEG recording of a motor
-# imagery experiment. The data was preprocessed to reduce eye movement artifacts
-# and resampled to a sampling rate of 100 Hz.
-# With a visual cue the subject was instructed to perform either hand of foot
-# motor imagery. The the trigger time points of the cues are stored in 'tr', and
-# 'cl' contains the class labels (hand: 1, foot: -1). Duration of the motor
-# imagery period was approximately 6 seconds.
+# The data set contains a continuous 45 channel EEG recording of a motor
+# imagery experiment. The data was preprocessed to reduce eye movement
+# artifacts and resampled to a sampling rate of 100 Hz. With a visual cue, the
+# subject was instructed to perform either hand or foot motor imagery. The
+# trigger time points of the cues are stored in 'triggers', and 'classes'
+# contains the class labels. Duration of the motor imagery period was
+# approximately six seconds.
 import scotdata.motorimagery as midata
+
 
 raweeg = midata.eeg
 triggers = midata.triggers
@@ -21,20 +22,21 @@ fs = midata.samplerate
 locs = midata.locations
 
 
-# Prepare the data
+# Prepare data
 #
-# Here we cut segments from 3s to 4s following each trigger out of the EEG. This
-# is right in the middle of the motor imagery period.
+# Here we cut out segments from 3s to 4s after each trigger. This is right in
+# the middle of the motor imagery period.
 data = scot.datatools.cut_segments(raweeg, triggers, 3 * fs, 4 * fs)
 
 
-# Set up the analysis object
+# Set up analysis object
 #
-# We simply choose a VAR model order of 35, and reduction to 4 components (that's not a lot!).
+# We simply choose a VAR model order of 35, and reduction to 4 components
+# (that's not a lot).
 ws = scot.Workspace({'model_order': 35}, reducedim=4, fs=fs, locations=locs)
 
 
-# Perform MVARICA and plot the components
+# Perform CSPVARICA and plot components
 ws.set_data(data, classes)
 ws.do_cspvarica()
 
@@ -42,11 +44,12 @@ p = ws.var_.test_whiteness(50)
 print('Whiteness:', p)
 
 # Configure plotting options
-ws.plot_f_range = [0, 30]       # Only show 0 - 30 Hz
-ws.plot_diagonal = 'S'          # Put spectral density plots on the diagonal
-ws.plot_outside_topo = True     # Plot topos above and to the left
+ws.plot_f_range = [0, 30]  # Only show 0-30 Hz
+ws.plot_diagonal = 'S'  # Put spectral density plots on the diagonal
+ws.plot_outside_topo = True  # Plot topos above and to the left
 
 fig = ws.plot_connectivity_topos()
+
 
 # Connectivity Analysis
 #
