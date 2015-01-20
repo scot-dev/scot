a8a8aaf5def60b3a22a55ec5a1b1697f42b1ff49
Clemens Brunner <clemens.brunner@gmail.com>
2015-01-20 10:02:56 +0100

    Extended setup.py.

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+from codecs import open
 
 
-versionfile = open('VERSION', 'r')
-ver = versionfile.read().strip()
-versionfile.close()
+with open('VERSION', encoding='utf-8') as version:
+    ver = version.read().strip()
 
-setup(name='scot',
-      version=ver,
-      description='Source Connectivity Toolbox',
-      author='Martin Billinger',
-      author_email='martin.billinger@tugraz.at',
-      url='https://github.com/scot-dev/scot',
-      packages=['scot', 'scot.eegtopo', 'scot.external'],
-      install_requires=['numpy >=1.7', 'scipy >=0.12'])
+setup(
+    name='scot',
+    version=ver,
+    description='Source Connectivity Toolbox',
+    url='https://github.com/scot-dev/scot',
+    author='SCoT Development Team',
+    author_email='martin.billinger@tugraz.at',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='source connectivity EEG MEG ICA'
+    packages=['scot', 'scot.eegtopo', 'scot.external'],
+    install_requires=['numpy >=1.7', 'scipy >=0.12'],
+    )
