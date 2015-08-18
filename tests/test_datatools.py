edc4353254276a24fd9ae1cabd6a7afde59d2715
mbillinger <flkazemakase@gmail.com>
2015-08-18 13:32:56 +0200

    Data Orientation: test_datatools.py

--- tests/test_datatools.py
+++ tests/test_datatools.py
@@ -1,6 +1,6 @@
 # Released under The MIT License (MIT)
 # http://opensource.org/licenses/MIT
-# Copyright (c) 2013 SCoT Development Team
+# Copyright (c) 2013-2015 SCoT Development Team
 
 import unittest
 
@@ -18,7 +18,7 @@ class TestDataMangling(unittest.TestCase):
 
     def test_cut_epochs(self):
         triggers = [100, 200, 300, 400, 500, 600, 700, 800, 900]
-        rawdata = np.random.randn(1000, 5)
+        rawdata = np.random.randn(5, 1000)
         rawcopy = rawdata.copy()
 
         start = -10
@@ -27,29 +27,29 @@ class TestDataMangling(unittest.TestCase):
         x = datatools.cut_segments(rawdata, triggers, -10, 50)
 
         self.assertTrue(np.all(rawdata == rawcopy))
-        self.assertEqual(x.shape, (stop - start, x.shape[1], len(triggers)))
+        self.assertEqual(x.shape, (len(triggers), rawdata.shape[0], stop - start))
 
         for it in range(len(triggers)):
-            a = rawdata[triggers[it] + start: triggers[it] + stop, :]
-            b = x[:, :, it]
+            a = rawdata[:, triggers[it] + start: triggers[it] + stop]
+            b = x[it, :, :]
             self.assertTrue(np.all(a == b))
 
     def test_cat_trials(self):
-        x = np.random.randn(60, 5, 9)
+        x = np.random.randn(9, 5, 60)
         xc = x.copy()
 
         y = datatools.cat_trials(x)
 
         self.assertTrue(np.all(x == xc))
-        self.assertEqual(y.shape, (x.shape[0] * x.shape[2], x.shape[1]))
+        self.assertEqual(y.shape, (x.shape[1], x.shape[0] * x.shape[2]))
 
-        for it in range(x.shape[2]):
-            a = y[it * x.shape[0]: (it + 1) * x.shape[0], :]
-            b = x[:, :, it]
+        for it in range(x.shape[0]):
+            a = y[:, it * x.shape[2]: (it + 1) * x.shape[2]]
+            b = x[it, :, :]
             self.assertTrue(np.all(a == b))
 
     def test_dot_special(self):
-        x = np.random.randn(60, 5, 9)
+        x = np.random.randn(9, 5, 60)
         a = np.eye(5) * 2.0
 
         xc = x.copy()
@@ -71,10 +71,10 @@ class TestRegressions(unittest.TestCase):
 
     def test_cat_trials_dimensions(self):
         """cat_trials did not always return a 2d array."""
-        self.assertEqual(datatools.cat_trials(np.random.randn(100, 2, 2)).ndim, 2)
-        self.assertEqual(datatools.cat_trials(np.random.randn(100, 1, 2)).ndim, 2)
-        self.assertEqual(datatools.cat_trials(np.random.randn(100, 2, 1)).ndim, 2)
-        self.assertEqual(datatools.cat_trials(np.random.randn(100, 1, 1)).ndim, 2)
+        self.assertEqual(datatools.cat_trials(np.random.randn(2, 2, 100)).ndim, 2)
+        self.assertEqual(datatools.cat_trials(np.random.randn(1, 2, 100)).ndim, 2)
+        self.assertEqual(datatools.cat_trials(np.random.randn(2, 1, 100)).ndim, 2)
+        self.assertEqual(datatools.cat_trials(np.random.randn(1, 1, 100)).ndim, 2)
 
 
 def main():
