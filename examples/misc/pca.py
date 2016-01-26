23219d8a093d098ceef4054982d9d44d131b78a1
Clemens Brunner <clemens.brunner@gmail.com>
2016-01-26 12:23:06 +0100

    Verify example (it works)

--- examples/misc/pca.py
+++ examples/misc/pca.py
@@ -1,10 +1,9 @@
 # Released under The MIT License (MIT)
 # http://opensource.org/licenses/MIT
-# Copyright (c) 2013-2015 SCoT Development Team
+# Copyright (c) 2013-2016 SCoT Development Team
 
-"""
-This example demonstrates that it is possible to reconstruct sources even if we
-include a PCA step in the process.
+"""This example demonstrates that it is possible to reconstruct sources even if
+we include PCA in the process.
 """
 
 from __future__ import print_function
@@ -27,6 +26,7 @@ x = model0.simulate(10000).squeeze()
 w, v = pca(x)
 y = np.dot(w.T, x)
 
+# Verify that transformed data y is decorrelated
 print('Covariance of x:\n', np.cov(x.squeeze()))
 print('\nCovariance of y:\n', np.cov(y.squeeze()))
 
@@ -45,3 +45,6 @@ print('\n(2) VAR coefficients estimated on x:\n', model1.coef)
 print('\n(3) VAR coefficients estimated on y:\n', model2.coef)
 print('\n(4) VAR coefficients estimated on y and transformed back:\n',
       w.dot(model2.coef).dot(w.T))
+
+print('\n(5) Check if (2) and (4) are equal:\n',
+      np.isclose(model1.coef, w.dot(model2.coef).dot(w.T)))
