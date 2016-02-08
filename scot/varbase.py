999088b5800d73cb9d8ca0201aab0393bcebb240
Clemens Brunner <clemens.brunner@gmail.com>
2016-02-08 13:41:13 +0100

    Unify _construct_var_eqns

--- scot/varbase.py
+++ scot/varbase.py
@@ -332,21 +332,25 @@ class VARBase(object):
 ############################################################################
 
 
-def _construct_var_eqns(data, p):
-        """ Construct VAR equation system
+def _construct_var_eqns(data, p, delta=None):
+        """Construct VAR equation system (optionally with RLS constraint).
         """
-        (t, m, l) = np.shape(data)
-        n = (l - p) * t     # number of linear relations
+        t, m, l = sp.shape(data)
+        n = (l - p) * t  # number of linear relations
+        rows = n if delta is None else n + m * p
+
         # Construct matrix x (predictor variables)
-        x = np.zeros((n, m * p))
+        x = np.zeros((rows, m * p))
         for i in range(m):
             for k in range(1, p + 1):
-                x[:, i * p + k - 1] = np.reshape(data[:, i, p - k:-k].T, n)
+                x[:n, i * p + k - 1] = np.reshape(data[:, i, p - k:-k].T, n)
+        if delta is not None:
+            np.fill_diagonal(x[n:, :], delta)
 
         # Construct vectors yi (response variables for each channel i)
-        y = np.zeros((n, m))
+        y = sp.zeros((rows, m))
         for i in range(m):
-            y[:, i] = np.reshape(data[:, i, p:].T, n)
+            y[:n, i] = sp.reshape(data[:, i, p:].T, n)
 
         return x, y
 
