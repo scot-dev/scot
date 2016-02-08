0919f1921e200c741adbb79f2e51325ca1984e1c
mbillinger <flkazemakase@gmail.com>
2016-02-08 14:37:20 +0100

    Fixed missing "

--- distributions/ci/run_tests.sh
+++ distributions/ci/run_tests.sh
@@ -6,7 +6,7 @@
 
 echo "============================"
 echo "============================"
-echo "Testing Environment:
+echo "Testing Environment:"
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
