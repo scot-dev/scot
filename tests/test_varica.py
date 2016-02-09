4b4f721535f5c734e02cf90aa236939a6f067a45
mbillinger <flkazemakase@gmail.com>
2016-02-09 11:59:32 +0100

    Added tests for VARICA submodel fitting

--- tests/test_varica.py
+++ tests/test_varica.py
@@ -26,6 +26,47 @@ class TestMVARICA(unittest.TestCase):
         varica.mvarica(np.sin(np.arange(30)).reshape((10, 3)), VAR(1))    # 10 samples, 3 channels
         varica.mvarica(np.sin(np.arange(30)).reshape((5, 3, 2)), VAR(1))  # 5 samples, 3 channels, 2 trials
 
+    def testFit(self):
+        """ Test submodel fitting on instationary data
+        """
+        np.random.seed(42)
+
+        # original model coefficients
+        b01 = np.array([[0.0, 0], [0, 0]])
+        b02 = np.array([[0.5, 0.3], [0.3, 0.5]])
+        b03 = np.array([[0.1, 0.1], [0.1, 0.1]])
+        t, m, l = 10, 2, 100
+
+        noisefunc = lambda: np.random.normal(size=(1, m)) ** 3 / 1e3
+
+        var = VAR(1)
+        var.coef = b01
+        sources1 = var.simulate([l, t], noisefunc)
+        var.coef = b02
+        sources2 = var.simulate([l, t], noisefunc)
+        var.coef = b03
+        sources3 = var.simulate([l, t * 2], noisefunc)
+
+        sources = np.vstack([sources1, sources2, sources3])
+        cl = [1] * t + [2] * t + [1, 2] * t
+
+        var = VAR(1)
+        r_trial = varica.mvarica(sources, var, cl, reducedim='no_pca', varfit='trial')
+        r_class = varica.mvarica(sources, var, cl, reducedim='no_pca', varfit='class')
+        r_ensemble = varica.mvarica(sources, var, cl, reducedim='no_pca', varfit='ensemble')
+
+        vars = [np.var(r.var_residuals) for r in [r_trial, r_class, r_ensemble]]
+
+        # class one consists of trials generated with b01 and b03
+        # class two consists of trials generated with b02 and b03
+        #
+        # ensemble fitting cannot resolve any model -> highest residual variance
+        # class fitting cannot only resolve (b01+b03) vs (b02+b03) -> medium residual variance
+        # trial fitting can resolve all three models -> lowest residual variance
+
+        self.assertLess(vars[0], vars[1])
+        self.assertLess(vars[1], vars[2])
+
     def testModelIdentification(self):
         """ generate VAR signals, mix them, and see if MVARICA can reconstruct the signals
             do this for every backend """
@@ -93,5 +134,47 @@ class TestCSPVARICA(unittest.TestCase):
         # simply pass in different data shapes and see if the functions runs without error
         self.assertRaises(AttributeError, varica.cspvarica, np.sin(np.arange(30)).reshape((10, 3)), VAR(1), [0])
         varica.cspvarica(np.sin(np.arange(30)).reshape((2, 3, 5)), VAR(1), ['A', 'B'])  # 5 samples, 3 channels, 2 trials
+
+    def testFit(self):
+        """ Test submodel fitting on instationary data
+        """
+        np.random.seed(42)
+
+        # original model coefficients
+        b01 = np.array([[0.0, 0], [0, 0]])
+        b02 = np.array([[0.5, 0.3], [0.3, 0.5]])
+        b03 = np.array([[0.1, 0.1], [0.1, 0.1]])
+        t, m, l = 10, 2, 100
+
+        noisefunc = lambda: np.random.normal(size=(1, m)) ** 3 / 1e3
+
+        var = VAR(1)
+        var.coef = b01
+        sources1 = var.simulate([l, t], noisefunc)
+        var.coef = b02
+        sources2 = var.simulate([l, t], noisefunc)
+        var.coef = b03
+        sources3 = var.simulate([l, t * 2], noisefunc)
+
+        sources = np.vstack([sources1, sources2, sources3])
+        cl = [1] * t + [2] * t + [1, 2] * t
+
+        var = VAR(1)
+        r_trial = varica.cspvarica(sources, var, cl, reducedim=None, varfit='trial')
+        r_class = varica.cspvarica(sources, var, cl, reducedim=None, varfit='class')
+        r_ensemble = varica.cspvarica(sources, var, cl, reducedim=None, varfit='ensemble')
+
+        vars = [np.var(r.var_residuals) for r in [r_trial, r_class, r_ensemble]]
+
+        # class one consists of trials generated with b01 and b03
+        # class two consists of trials generated with b02 and b03
+        #
+        # ensemble fitting cannot resolve any model -> highest residual variance
+        # class fitting cannot only resolve (b01+b03) vs (b02+b03) -> medium residual variance
+        # trial fitting can resolve all three models -> lowest residual variance
+        print(vars)
+
+        self.assertLess(vars[0], vars[1])
+        self.assertLess(vars[1], vars[2])
 
 
