d7127f60f8d360ab4e4d0ffb62cf5203e772a800
Martin Billinger <martin.billinger@tugraz.at>
2016-02-11 14:57:31 +0100

    Fixed doctest

--- scot/datatools.py
+++ scot/datatools.py
@@ -144,14 +144,14 @@ def randomize_phase(data):
         from pylab import *
         from scot.datatools import randomize_phase
         np.random.seed(1234)
-        s = np.sin(np.linspace(0,10*np.pi,1000)).T
-        x = np.vstack([s, np.sign(s)]).T
+        s = np.sin(np.linspace(0,10*np.pi,1000))
+        x = np.vstack([s, np.sign(s)])
         y = randomize_phase(x)
         subplot(2,1,1)
         title('Phase randomization of sine wave and rectangular function')
-        plot(x), axis([0,1000,-3,3])
+        plot(x.T + [1.5, -1.5]), axis([0,1000,-3,3])
         subplot(2,1,2)
-        plot(y), axis([0,1000,-3,3])
+        plot(y.T + [1.5, -1.5]), axis([0,1000,-3,3])
         plt.show()
     """
     data = np.asarray(data)
