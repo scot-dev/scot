a70cca2ee61fe3db405137900718806d541a2426
Clemens Brunner <clemens.brunner@gmail.com>
2016-02-16 09:09:03 +0100

    Move tests and adapt Travis script

--- distributions/ci/run_tests.sh
+++ distributions/ci/run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 # This script is meant to be called by the "script" step defined in
 # .travis.yml. See http://docs.travis-ci.com/ for more details.
-# The behavior of the script is controlled by environment variabled defined
+# The behavior of the script is controlled by environment variables defined
 # in the .travis.yml in the top level folder of the project.
 
 echo "============================"
@@ -18,7 +19,7 @@ echo "============================"
 
 if [[ "$INSTALL_SCOT" == "true" ]]; then
     python setup.py install
-    cd tests
+    cd scot/tests
 fi
 
 if [[ "$COVERAGE" == "true" ]]; then
@@ -29,7 +30,7 @@ fi
 
 if [[ "$RUN_EXAMPLES" == "true" ]]; then
     if [[ "$INSTALL_SCOT" == "true" ]]; then
-        xvfb-run --server-args="-screen 0 1024x768x24" find ../examples -type f -iname "*\.py" -exec python {} \;
+        xvfb-run --server-args="-screen 0 1024x768x24" find ../../examples -type f -iname "*\.py" -exec python {} \;
     else
         PYTHONPATH=. xvfb-run --server-args="-screen 0 1024x768x24" find examples -type f -iname "*\.py" -exec python {} \;
     fi
